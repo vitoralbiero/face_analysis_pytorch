cb67e8574d7b4ddf87ea0a0ad4666bc3eea36cb2
Vitor Albiero <vitor_albiero@outlook.com>
2020-04-01 20:48:16 -0400

    Fixes on model loader and count classes

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -3,33 +3,29 @@ from .utils import get_time
 from os import path
 
 
-def save_state(model, head, optimizer, config, accuracy,
-               to_save_folder=False, step=0, model_only=False):
-    if to_save_folder:
-        save_path = config.save_path
-    else:
-        save_path = config.model_path
+def save_state(model, head, optimizer, config, accuracy, step=0, model_only=False):
+    save_path = config.model_path
 
     torch.save(
         model.state_dict(), path.join(save_path,
-                                      'model_{}_accuracy:{}_step:{}.pth'.format(get_time(),
-                                                                                accuracy,
-                                                                                step)))
+                                      'model_{}_accuracy:{:.4f}_step:{}.pth'.format(get_time(),
+                                                                                    accuracy,
+                                                                                    step)))
 
     if not model_only:
         torch.save(
             head.state_dict(), path.join(save_path,
-                                         'head_{}_accuracy:{}_step:{}.pth'.format(get_time(),
-                                                                                  accuracy,
-                                                                                  step)))
+                                         'head_{}_accuracy:{:.4f}_step:{}.pth'.format(get_time(),
+                                                                                      accuracy,
+                                                                                      step)))
         torch.save(
             optimizer.state_dict(), path.join(save_path,
-                                              'optimizer_{}_accuracy:{}_step:{}.pth'.format(get_time(),
-                                                                                            accuracy,
-                                                                                            step)))
+                                              'optimizer_{}_accuracy:{:.4f}_step:{}.pth'.format(get_time(),
+                                                                                                accuracy,
+                                                                                                step)))
 
 
-def load_state(model, head, optimizer, config, path_to_model, model_only=False, load_head=True):
+def load_state(model, head, optimizer, path_to_model, model_only=False, load_head=True):
     model.load_state_dict(torch.load(path_to_model))
 
     if load_head or not model_only:
