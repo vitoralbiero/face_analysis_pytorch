f30846ebfc3f98c7cd71ad43a891556bfba0a855
Vitor Albiero <vitor_albiero@outlook.com>
2020-04-01 20:45:42 -0400

    changed from LR to OR

--- main.py
+++ main.py
@@ -7,31 +7,33 @@ import numpy as np
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train a race, gender, or age predictor.')
-    parser.add_argument('-e', '--epochs', help='Number of epochs.', default=30, type=int)
-    parser.add_argument('-n', '--net', help='Residual type [ir, ir_se].', default='ir_se', type=str)
-    parser.add_argument('-d', '--depth', help='Number of layers [50, 100, 152].', default=50, type=int)
-    parser.add_argument('-lr', '--lr', help='Learning rate.', default=0.001, type=float)
-    parser.add_argument('-b', '--batch_size', help='Batch size.', default=384, type=int)
-    parser.add_argument('-w', '--workers', help='Workers number.', default=4, type=int)
-    parser.add_argument('-m', '--multi_gpu', help='Use multi gpus.', type=bool, default=True)
+    parser.add_argument('--epochs', '-e', help='Number of epochs.', default=30, type=int)
+    parser.add_argument('--net_mode', '-n', help='Residual type [ir, ir_se].', default='ir_se', type=str)
+    parser.add_argument('--depth', '-d', help='Number of layers [50, 100, 152].', default=50, type=int)
+    parser.add_argument('--lr', '-lr', help='Learning rate.', default=0.001, type=float)
+    parser.add_argument('--batch_size', '-b', help='Batch size.', default=384, type=int)
+    parser.add_argument('--lr_plateau', '-lrp', help='Reduce lr on plateau.', type=bool, default=True)
+    parser.add_argument('--workers', '-w', help='Workers number.', default=4, type=int)
+    parser.add_argument('--multi_gpu', '-m', help='Use multi gpus.', type=bool, default=True)
 
-    parser.add_argument('-t', '--train_list', help='List of images to train.')
-    parser.add_argument('-v', '--val_list', help='List of images to validate.')
-    parser.add_argument('-ts', '--train_source', help='Path to the train images.')
-    parser.add_argument('-vs', '--val_source', help='Path to the val images.')
-    parser.add_argument('-a', '--attribute', help='Which attribute to train [race, gender, age].', type=str)
-    parser.add_argument('-p', '--prefix', help='Prefix to save the model.', type=str)
+    parser.add_argument('--train_list', '-t', help='List of images to train.')
+    parser.add_argument('--val_list', '-v', help='List of images to validate.')
+    parser.add_argument('--train_source', '-ts', help='Path to the train images.')
+    parser.add_argument('--val_source', '-vs', help='Path to the val images.')
+    parser.add_argument('--attribute', '-a', help='Which attribute to train [race, gender, age].', type=str)
+    parser.add_argument('--prefix', '-p', help='Prefix to save the model.', type=str)
 
-    parser.add_argument('-pt', '--pretrained', help='Path to pretrained weights.', type=str)
-    parser.add_argument('-r', '--resume', help='Path to load model to resume training.', type=str)
+    parser.add_argument('--pretrained', '-pt', help='Path to pretrained weights.', type=str)
+    parser.add_argument('--resume', '-r', help='Path to load model to resume training.', type=str)
 
     args = parser.parse_args()
 
     config = Config(args.prefix, args.attribute.lower())
 
-    config.net = args.net
+    config.net_mode = args.net_mode
     config.depth = args.depth
     config.lr = args.lr
+    config.lr_plateau = args.lr_plateau
     config.batch_size = args.batch_size
     config.workers = args.workers
     config.train_list = args.train_list
--- model/age_head.py
+++ model/age_head.py
@@ -1,12 +1,14 @@
 from torch.nn import Linear, Module
+from torch import sigmoid
 
 
 class AgeHead(Module):
     def __init__(self):
         super(AgeHead, self).__init__()
-        self.fc1 = Linear(512, 1)
+        self.fc1 = Linear(512, 100)
 
     def forward(self, x):
         x = self.fc1(x)
+        x = sigmoid(x)
 
-        return x.squeeze()
+        return x
