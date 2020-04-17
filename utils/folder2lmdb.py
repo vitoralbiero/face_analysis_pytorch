b3801a26a0fbc4bd2d9a6878781aed8a4ed2a117
Vitor Albiero <vitor_albiero@outlook.com>
2020-04-17 14:30:37 -0400

    Added folder 2 lmdb

--- utils/folder2lmdb.py
+++ utils/folder2lmdb.py
@@ -3,61 +3,26 @@ import lmdb
 import pyarrow as pa
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
-import numpy as np
-import pandas as pd
 from tqdm import tqdm
 import lz4framed
 
 
-class ImageListRaw(ImageFolder):
-    ATTRIBUTES = {'race': 1, 'gender': 2, 'age': 3, 'recognition': 1}
-    MAX_CLASS = {'race': 4, 'gender': 1, 'age': 100, 'recognition': float('Inf')}
-
-    def __init__(self, attribute_name, source, image_list, train=True):
-        attribute = self.ATTRIBUTES[attribute_name]
-        max_class = self.MAX_CLASS[attribute_name]
-
-        image_names = pd.read_csv(image_list, delimiter=' ', header=None)
-        image_names = np.asarray(image_names)
-
-        # remove images that have labels outside of desired range
-        image_names = image_names[image_names[:, attribute].astype('int') >= 0]
-        image_names = image_names[image_names[:, attribute].astype('int') <= max_class]
-
-        if source is not None:
-            self.samples = [path.join(source, image_name) for image_name in image_names[:, 0]]
-        else:
-            self.samples = image_names[:, 0]
-
-        self.targets = image_names[:, attribute].astype('int')
-        self.classnum = np.max(self.targets) + 1
-
-    def __len__(self):
-        return len(self.samples)
-
-    def __getitem__(self, index):
-        with open(self.samples[index], 'rb') as f:
-            img = f.read()
-
-        return img, self.targets[index]
-
-
-class CustomRawLoader(DataLoader):
-    def __init__(self, attribute, workers, source, image_list):
-        self._dataset = ImageListRaw(attribute, source, image_list)
-
-        super(CustomRawLoader, self).__init__(self._dataset, num_workers=workers, collate_fn=lambda x: x)
+def raw_reader(path):
+    with open(path, 'rb') as f:
+        bin_data = f.read()
+    return bin_data
 
 
 def dumps_pyarrow(obj):
     return lz4framed.compress(pa.serialize(obj).to_buffer())
 
 
-def list2lmdb(attribute, source, image_list, dest, num_workers=16, write_frequency=5000):
-    print("Loading dataset from %s" % image_list)
-    data_loader = CustomRawLoader(attribute, num_workers, source, image_list)
+def list2lmdb(attribute, source, dest, num_workers=16, write_frequency=5000):
+    print("Loading dataset from %s" % source)
+    dataset = ImageFolder(source, loader=raw_reader)
+    data_loader = DataLoader(dataset, num_workers=num_workers, collate_fn=lambda x: x)
 
-    name = f"{path.split(image_list)[1][:-4]}.lmdb"
+    name = f"{path.split(source)[1][:-4]}.lmdb"
     lmdb_path = path.join(dest, name)
     isdir = path.isdir(lmdb_path)
 
